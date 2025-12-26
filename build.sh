604a7cfa36081ad5fb358f9724a459314653e146
strong568 <cuongdieu173@gmail.com>
2025-12-26 20:34:41 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -328,7 +328,8 @@ mkdir -p anykernel/kernels/
 # Patch for SukiSU KPM support. 
 if [ $KSU_ENABLE -eq 1 ]; then
     cd out/arch/arm64/boot/
-    wget https://github.com/SukiSU-Ultra/SukiSU_KernelPatch_patch/releases/download/0.12.2/patch_linux
+    # wget https://github.com/SukiSU-Ultra/SukiSU_KernelPatch_patch/releases/download/0.12.2/patch_linux
+    wget https://github.com/SukiSU-Ultra/SukiSU_KernelPatch_patch/releases/latest/download/patch_linux
     chmod +x patch_linux
     ./patch_linux
     rm Image
